5aad49cf47bf6e78ddf8fd9ad67f0d5617ac6397
Jacob Dachenhaus <dachendev@gmail.com>
2024-07-19 18:39:15 -0400

    feat(Gameboard): Render Gameboards

--- src/main.js
+++ src/main.js
@@ -3,28 +3,27 @@ import Ship from "./lib/Ship.js";
 import "./main.css";
 import * as domUtils from "./utils/dom.utils.js";
 
-// create players
-const player1 = new Player();
-const player2 = new Player();
+// create player and computer
+const player = new Player();
+const computer = new Player();
 
 function setupGame() {
-  // populate gameboard for player 1
-  player1.gameboard.placeShip(new Ship(5), 0, 0);
-  player1.gameboard.placeShip(new Ship(4), 1, 0);
-  player1.gameboard.placeShip(new Ship(3), 2, 0);
-  player1.gameboard.placeShip(new Ship(3), 3, 0);
-  player1.gameboard.placeShip(new Ship(2), 4, 0);
+  // populate gameboard for player
+  player.gameboard.placeShip(new Ship(5), 0, 0);
+  player.gameboard.placeShip(new Ship(4), 1, 0);
+  player.gameboard.placeShip(new Ship(3), 2, 0);
+  player.gameboard.placeShip(new Ship(3), 3, 0);
+  player.gameboard.placeShip(new Ship(2), 4, 0);
 
-  // populate gameboard for player 2
-  player2.gameboard.placeShip(new Ship(5), 0, 0);
-  player2.gameboard.placeShip(new Ship(4), 1, 0);
-  player2.gameboard.placeShip(new Ship(3), 2, 0);
-  player2.gameboard.placeShip(new Ship(3), 3, 0);
-  player2.gameboard.placeShip(new Ship(2), 4, 0);
+  // populate gameboard for computer
+  computer.gameboard.placeShip(new Ship(5), 0, 0);
+  computer.gameboard.placeShip(new Ship(4), 1, 0);
+  computer.gameboard.placeShip(new Ship(3), 2, 0);
+  computer.gameboard.placeShip(new Ship(3), 3, 0);
+  computer.gameboard.placeShip(new Ship(2), 4, 0);
 
-  // render gameboards
-  domUtils.renderGameboard(player1.gameboard);
-  domUtils.renderGameboard(player2.gameboard);
+  // render player gameboard
+  domUtils.renderGameboard(player.gameboard);
 }
 
 function domLoaded() {
--- src/utils/dom.utils.js
+++ src/utils/dom.utils.js
@@ -1,1 +1,20 @@
-export function renderGameboard(gameboard) {}
+export function renderGameboard(gameboard) {
+  const gameboardElem = document.createElement("div");
+  gameboardElem.classList.add("gameboard");
+
+  for (let y = 0; y < gameboard.grid.length; y++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    gameboardElem.appendChild(row);
+
+    for (let x = 0; x < gameboard.grid[y].length; x++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.dataset.x = x;
+      cell.dataset.y = y;
+      row.appendChild(cell);
+    }
+  }
+
+  document.getElementById("root").appendChild(gameboardElem);
+}
