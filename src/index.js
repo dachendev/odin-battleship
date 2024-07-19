b19c038b22d44f238cdb2117559c0893fed3805a
Jacob Dachenhaus <dachendev@gmail.com>
2024-07-19 18:24:48 -0400

    feat: Set up game

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import Ship from "./lib/Ship";
 import Gameboard from "./lib/Gameboard";
 import Player from "./lib/Player";
+import * as domUtils from "./dom-utils";
 
-export { Ship, Gameboard, Player };
+// create players
+const player1 = new Player();
+const player2 = new Player();
+
+function setupGame() {
+  // populate gameboard for player 1
+  player1.gameboard.placeShip(new Ship(5), 0, 0);
+  player1.gameboard.placeShip(new Ship(4), 1, 0);
+  player1.gameboard.placeShip(new Ship(3), 2, 0);
+  player1.gameboard.placeShip(new Ship(3), 3, 0);
+  player1.gameboard.placeShip(new Ship(2), 4, 0);
+
+  // populate gameboard for player 2
+  player2.gameboard.placeShip(new Ship(5), 0, 0);
+  player2.gameboard.placeShip(new Ship(4), 1, 0);
+  player2.gameboard.placeShip(new Ship(3), 2, 0);
+  player2.gameboard.placeShip(new Ship(3), 3, 0);
+  player2.gameboard.placeShip(new Ship(2), 4, 0);
+
+  // render gameboards
+  domUtils.renderGameboard(player1.gameboard);
+  domUtils.renderGameboard(player2.gameboard);
+}
+
+setupGame();
