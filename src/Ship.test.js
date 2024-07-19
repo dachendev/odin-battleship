d4568c35e903e0aec5c029e379ac322c6497cdf1
Jacob Dachenhaus <dachendev@gmail.com>
2024-07-19 17:14:52 -0400

    feat(Ship): Add Ship and tests

---
+++ src/Ship.test.js
@@ -0,0 +1,32 @@
+import Ship from "./Ship";
+
+describe("Ship", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(3);
+  });
+
+  test("should create a ship", () => {
+    expect(ship).toHaveProperty("length");
+    expect(ship).toHaveProperty("hits");
+  });
+
+  test("should take hits", () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+    ship.hit();
+    expect(ship.hits).toBe(2);
+    ship.hit();
+    expect(ship.hits).toBe(3);
+    ship.hit();
+    expect(ship.hits).toBe(3);
+  });
+
+  test("should sink", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
