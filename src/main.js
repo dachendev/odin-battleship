229f8f821c4d47592199f786730c640b21a53091
Jacob Dachenhaus <dachendev@gmail.com>
2024-07-20 13:38:55 -0400

    feat(gameboard): display gameboard and ships

--- src/main.js
+++ src/main.js
@@ -10,20 +10,21 @@ const computer = new Player();
 function setupGame() {
   // populate gameboard for player
   player.gameboard.placeShip(new Ship(5), 0, 0);
-  player.gameboard.placeShip(new Ship(4), 1, 0);
-  player.gameboard.placeShip(new Ship(3), 2, 0);
-  player.gameboard.placeShip(new Ship(3), 3, 0);
-  player.gameboard.placeShip(new Ship(2), 4, 0);
+  player.gameboard.placeShip(new Ship(4), 0, 1);
+  player.gameboard.placeShip(new Ship(3), 0, 2);
+  player.gameboard.placeShip(new Ship(3), 0, 3);
+  player.gameboard.placeShip(new Ship(2), 0, 4);
 
   // populate gameboard for computer
   computer.gameboard.placeShip(new Ship(5), 0, 0);
-  computer.gameboard.placeShip(new Ship(4), 1, 0);
-  computer.gameboard.placeShip(new Ship(3), 2, 0);
-  computer.gameboard.placeShip(new Ship(3), 3, 0);
-  computer.gameboard.placeShip(new Ship(2), 4, 0);
+  computer.gameboard.placeShip(new Ship(4), 0, 1);
+  computer.gameboard.placeShip(new Ship(3), 0, 2);
+  computer.gameboard.placeShip(new Ship(3), 0, 3);
+  computer.gameboard.placeShip(new Ship(2), 0, 4);
 
   // render player gameboard
-  domUtils.renderGameboard(player.gameboard);
+  domUtils.renderGameboard(player.gameboard, true);
+  domUtils.renderGameboard(computer.gameboard);
 }
 
 function domLoaded() {
--- src/utils/dom.utils.js
+++ src/utils/dom.utils.js
@@ -1,11 +1,15 @@
-export function renderGameboard(gameboard) {
+export function renderGameboard(gameboard, displayShips = false) {
   const gameboardElem = document.createElement("div");
   gameboardElem.classList.add("gameboard");
+
+  const gameboardContent = document.createElement("div");
+  gameboardContent.classList.add("gameboard-content");
+  gameboardElem.appendChild(gameboardContent);
 
   for (let y = 0; y < gameboard.height; y++) {
     const row = document.createElement("div");
     row.classList.add("row");
-    gameboardElem.appendChild(row);
+    gameboardContent.appendChild(row);
 
     for (let x = 0; x < gameboard.width; x++) {
       const cell = document.createElement("div");
@@ -13,6 +17,10 @@ export function renderGameboard(gameboard) {
       cell.dataset.x = x;
       cell.dataset.y = y;
       row.appendChild(cell);
+
+      if (displayShips && !!gameboard.getShip(x, y)) {
+        cell.classList.add("has-ship");
+      }
     }
   }
 
